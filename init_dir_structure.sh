572362233fe567ccc2ab13f29a550bd97ba2fdcc
CORaisch <claudio.raisch@hotmail.de>
2019-07-29 11:47:59 +0200

    reworked directory skeleton

--- init_dir_structure.sh
+++ init_dir_structure.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
 ## clear directory 'raw'
-echo "clear directory 'raw'"
+echo "init directory 'raw'"
 # remove directory structure if exists
 if [ -d "raw" ]; then
     rm -rf raw
 fi
-# build directory structure for 'raw'
+
+# build directory skeleton for 'raw'
 mkdir -p raw/stereo/left/images
 mkdir -p raw/stereo/right/images
-mkdir raw/stereo/left/timestamps
-mkdir raw/stereo/right/timestamps
-
-## clear directory 'processed'
-echo "clear directory 'processed'"
-# remove directory structure if exists
-if [ -d "processed" ]; then
-    rm -rf processed
-fi
-# build directory structure for 'processed'
-mkdir processed
+mkdir -p raw/depth/images
+mkdir -p raw/semseg/images
